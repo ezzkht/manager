1f2872eb5e26312276fb0a95a1d0d0a6e6988ad8
Ezz Khayyat <ekhayyat@gmail.com>
2019-01-05 18:49:33 +0200

    Added communications

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,13 +1,27 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
+import communications from 'react-native-communications';
 import {Card, CardSection, Button} from './common';
 import {connect} from 'react-redux';
-//import {employeeEdit} from '../actions';
+import {employeeUpdate, employeeSave} from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
     onButtonPress() {
         const {name, phone, shift} = this.props;
-        this.props.employeeCreate({name, phone, shift: shift || 'Monday'});
+        this.props.employeeSave({name, phone, shift, uid: this.props.employee.uid});
+    }
+
+    onTextPress() {
+        const {phone, shift} = this.props;
+
+        communications.text(phone, `Your upcoming shift is on ${shift}`);
+    }
+
+    componentWillMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({prop, value});
+        });
     }
 
     render() {
@@ -17,6 +31,9 @@ class EmployeeEdit extends Component {
                 <CardSection>
                     <Button
                     onPress={this.onButtonPress.bind(this)}>Save Changes</Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
                 </CardSection>
             </Card>
         )
@@ -29,4 +46,4 @@ const mapStateToProps = (state) => {
     return {name, phone, shift};
 };
 
-export default connect(mapStateToProps)(EmployeeEdit);
+export default connect(mapStateToProps, {employeeUpdate, employeeSave})(EmployeeEdit);
