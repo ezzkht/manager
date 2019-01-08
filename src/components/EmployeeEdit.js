471909af0ec19debe7ce1c6b7f43683b91b04efe
Ezz Khayyat <ekhayyat@gmail.com>
2019-01-08 23:28:23 +0200

    Added modal + fire employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import communications from 'react-native-communications';
-import {Card, CardSection, Button} from './common';
+import {Card, CardSection, Button, Confirm} from './common';
 import {connect} from 'react-redux';
-import {employeeUpdate, employeeSave} from '../actions';
+import {employeeUpdate, employeeSave, employeeDelete} from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+    state = { showModal: false };
+
     onButtonPress() {
         const {name, phone, shift} = this.props;
         this.props.employeeSave({name, phone, shift, uid: this.props.employee.uid});
@@ -16,6 +18,19 @@ class EmployeeEdit extends Component {
         const {phone, shift} = this.props;
 
         communications.text(phone, `Your upcoming shift is on ${shift}`);
+    }
+
+    onFirePress() {
+        this.setState({showModal: !this.state.showModal});
+    }
+
+    onAccept() {
+        const {uid} = this.props.employee;
+        this.props.employeeDelete({uid});
+    }
+
+    onDecline() {
+        this.setState({showModal: false});
     }
 
     componentWillMount() {
@@ -35,6 +50,17 @@ class EmployeeEdit extends Component {
                 <CardSection>
                     <Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
                 </CardSection>
+                <CardSection>
+                    <Button onPress={this.onFirePress.bind(this)}>Fire Employee</Button>
+                </CardSection>
+
+                <Confirm 
+                    visible={this.state.showModal}
+                    onAccept={this.onAccept.bind(this)}
+                    onDecline={this.onDecline.bind(this)}
+                >
+                    Are you sure you want to delete this?
+                </Confirm>
             </Card>
         )
     }
@@ -46,4 +72,4 @@ const mapStateToProps = (state) => {
     return {name, phone, shift};
 };
 
-export default connect(mapStateToProps, {employeeUpdate, employeeSave})(EmployeeEdit);
+export default connect(mapStateToProps, {employeeUpdate, employeeSave, employeeDelete})(EmployeeEdit);
